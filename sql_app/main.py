30e2524d7027bfea09560bc973a5cc7fd773789f
MiaYinel <103024412+HAnh101@users.noreply.github.com>
2023-07-08 02:21:14 +0700

    Update main.py

--- sql_app/main.py
+++ sql_app/main.py
@@ -2,54 +2,49 @@ from typing import List
 
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
-
+import appDes as appDes
 from . import data, models, schemas
 from .database import SessionLocal, engine
-
+from fastapi.responses import HTMLResponse
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(
+    title="Quản lý luơng nhân viên",
+    description=appDes.description,
+    version="1.0.0",
+    contact={
+        "name": "Source Code",
+        "url": "https://github.com/HAnh101/BTL_Python_K3N2N2023",
+    },
+    openapi_tags=appDes.tags_metadata
+)
 
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = data.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return data.create_user(db=db, user=user)
-
-
-@app.get("/users/", response_model=List[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = data.get_users(db, skip=skip, limit=limit)
-    return users
-
-
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = data.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
-
-@app.post("/users/{user_id}/items/", response_model=schemas.Item)
-def create_item_for_user(
-    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-):
-    return data.create_user_item(db=db, item=item, user_id=user_id)
-
-
-@app.get("/items/", response_model=List[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = data.get_items(db, skip=skip, limit=limit)
-    return items
+@app.get('/', response_class=HTMLResponse, tags=['Trang chủ'])
+def home():
+    html_content = '''
+    <html>
+        <head>
+            <title>BTL Python K3N2</title>
+            <style>
+                h1{
+                    text-align: center;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>Bài tập lớn môn Lập trình Python</h1>
+            <br> </br>
+            <div>
+                <h2>Nhóm gồm 4 thành viên:</h2>
+                <ul>
+                    <li>A39482 Nguyễn Ngọc Anh</li>
+                    <li>A38303 Phùng Thị Diệu Linh</li>
+                    <li>A38207 Nguyễn Thị Lan</li>
+                    <li>A37672 Trần Hoàng Anh</li>
+                </ul>
+            </div>
+        </body>
+    </html>
+    '''
+    return HTMLResponse(content=html_content, status_code=200)
